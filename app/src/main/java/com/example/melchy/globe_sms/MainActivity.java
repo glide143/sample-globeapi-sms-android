88823be3fb4db78bb5140c06a12516c82034d7c1
Glide143 <melchizedekaguinaldo@yahoo.com>
2017-07-21 10:14:34 +0800

    Update MainActivity

--- app/src/main/java/com/example/melchy/globe_sms/MainActivity.java
+++ app/src/main/java/com/example/melchy/globe_sms/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.melchy.globe_sms;
 
 import android.app.ProgressDialog;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Context;
 
 import ph.com.globe.connect.*;
 import org.json.JSONException;
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private Sms sms;
     private TextView tvResult;
     private EditText etMessage;
-    public  String TAG = "MainActivity";
+    public  String TAG = "MainActivityLog";
     private String message;
     ProgressDialog mProgressDialog;
     @Override
@@ -31,6 +34,13 @@ public class MainActivity extends AppCompatActivity {
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setTitle("Globe API SMS");
     }
+    //Check for internet connection
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
 
     public void sendMessage (View view) {
         message = etMessage.getText().toString();
@@ -39,31 +49,37 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mProgressDialog.show();
             mProgressDialog.setMessage("Sending Message");
-            try {
-                sms
-                        .setClientCorrelator("1234")
-                        .setReceiverAddress("YOUR_SUBSCRIBERS_NUMBER")
-                        .setMessage(etMessage.getText().toString())
-                        .sendMessage(new AsyncHandler() {
-                            @Override
-                            public void response(HttpResponse response) throws HttpResponseException {
-                                try {
-                                    JSONObject json = new JSONObject(response.getJsonResponse().toString());
-                                    Log.d(TAG,json.toString());
-                                    mProgressDialog.dismiss();
-                                    etMessage.setText("");
-                                    Toast.makeText(MainActivity.this, "Message has been sent", Toast.LENGTH_SHORT).show();
-                                } catch (JSONException e) {
-                                    Log.d(TAG,e.getMessage());
-                                }
-                            }
-                        });
+                if(isNetworkAvailable()){
+                    try {
+                        sms
+                                .setClientCorrelator("1234")
+                                .setReceiverAddress("YOUR_SUBSCRIBERS_NUMBER")
+                                .setMessage(etMessage.getText().toString())
+                                .sendMessage(new AsyncHandler() {
+                                    @Override
+                                    public void response(HttpResponse response) throws HttpResponseException {
+                                        try {
+                                            JSONObject json = new JSONObject(response.getJsonResponse().toString());
+                                            Log.d(TAG,json.toString());
+                                            mProgressDialog.dismiss();
+                                            etMessage.setText("");
+                                            Toast.makeText(MainActivity.this, "Message has been sent", Toast.LENGTH_SHORT).show();
+                                        } catch (JSONException e) {
+                                            Log.d(TAG,e.getMessage());
+                                        }
+                                    }
+                                });
 
-            } catch (ApiException | HttpRequestException | HttpResponseException e) {
-               Log.d(TAG,e.getMessage());
-            }
+                    } catch (ApiException | HttpRequestException | HttpResponseException e) {
+                        Log.d(TAG,e.getMessage());
+                    }
+                }else{
+                    mProgressDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "No Internet Connection", Toast.LENGTH_SHORT).show();
+                }
 
         }
+
     }
 
 
